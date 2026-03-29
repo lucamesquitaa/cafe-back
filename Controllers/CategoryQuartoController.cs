83dde22cb56e346920895f67227070f671b61014
Luca A Mesquita <lucamesquitaa@gmail.com>
2026-03-29 13:52:56 -0300

    melhorias

--- Controllers/CategoryQuartoController.cs
+++ Controllers/CategoryQuartoController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Turify.Facades;
+using Turify.Facades.Interfaces;
 using Turify.Models;
+using Turify.Models.DTOs;
 
 namespace Turify.Controllers
 {
@@ -28,9 +31,27 @@ namespace Turify.Controllers
         return Ok(result);
     }
 
+    [HttpGet("GetById/{id}")]
+    public async Task<IActionResult> GetById(string id)
+    {
+      try
+      {
+        var result = await _ctQuartoFacade.GetCategoryQuartoById(id);
+
+        if (result == null)
+          return BadRequest("Tipo de quarto não encontrado.");
+
+        return Ok(result);
+      }
+      catch (Exception e)
+      {
+        throw new Exception("Erro ao buscar tipo de quarto: " + e.Message);
+      }
+    }
+
     // POST: CategoryQuartoController/Edit/5
     [HttpPost("PostCategoryQuarto/{hotelId}")]
-    public async Task<IActionResult> PostQuartos([FromBody] CategoryQuarto obj, string hotelId)
+    public async Task<IActionResult> PostQuartos([FromBody] CriarCategoryQuartoDTO obj, string hotelId)
     {
       var result = await _ctQuartoFacade.PostCategoryQuartos(obj, hotelId);
 
@@ -40,6 +61,20 @@ namespace Turify.Controllers
         return Ok(result);
 
     }
+
+    [HttpPut("PutCategoryQuarto/{id}")]
+    [Authorize]
+    public async Task<IActionResult> PutCategoryQuarto(
+    string id, [FromBody] CategoryQuarto dto)
+    {
+      var result = await _ctQuartoFacade.PutCategoryQuartos(id, dto);
+
+      if (result.Sucesso == false)
+        return BadRequest(result);
+      else
+        return Ok(result);
+    }
+
 
     // GET: CategoryQuartoController/Delete/5
     [HttpDelete("{id}")]
