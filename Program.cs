2e6bc38b72897bb76ac1ac3b2a6f9b34970823cf
Luca A Mesquita <lucamesquitaa@gmail.com>
2026-03-29 19:30:45 -0300

    turufy

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddCors(options =>
 {
  options.AddPolicy("AllowAngular",
  policy => policy
- .WithOrigins("http://localhost:4200", "https://turify.com.br", "https://www.turify.com.br", "https://api.turify.com.br")
+ .WithOrigins("http://localhost:4200","https://www.turify.com.br", "https://api.turify.com.br")
  .AllowAnyHeader()
  .AllowAnyMethod()
  .AllowCredentials());
